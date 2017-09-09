b04ec54dc17334ea6299d2513dd2656cf870548e
Tsalapova Maria <tsalapovadisco@gmail.com>
2017-09-09 17:00:36 +0300

    Create BRVTest.java

---
+++ simulation-and-statistical-modeling/01-basic-random-variable/BRVTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author CalapovaMD
+ * @version 1.0, 9/5/2017
+ */
+public abstract class BRVTest {
+    static final double epsilon=0.05;
+    static final double Delta=1.96;
+
+    public abstract void run(BRVMethod method);
+}
